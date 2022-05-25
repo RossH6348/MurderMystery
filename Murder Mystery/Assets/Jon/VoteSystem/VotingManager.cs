c58821268891fae13314a0b485b2eddb6ff0a4a4
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2022-05-25 15:00:17 +0100

    Brought in more stuff for voting system

---
+++ Murder Mystery/Assets/Jon/VoteSystem/VotingManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VotingManager : MonoBehaviour
+{
+    private int maxVotes = 3;
+    private int votesRemaining;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
