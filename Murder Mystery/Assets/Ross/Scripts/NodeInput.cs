f0b015c46af2ed2615f2e63e87084b5bff2f1e41
RossH6348 <75311103+RossH6348@users.noreply.github.com>
2022-05-25 14:03:18 +0100

    VR Path Preview update.
    Should change red when a path is longer than what they rolled. And a number to show many spaces it is.

--- Murder Mystery/Assets/Ross/Scripts/NodeInput.cs
+++ Murder Mystery/Assets/Ross/Scripts/NodeInput.cs
@@ -1,30 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeInput : LaserInput
 {
 
     private List<GameObject> currentPath = null;
+    [SerializeField] private GameObject pathNumber;
+
     public override void onLaserSelected(CharacterScript character)
     {
+
         currentPath = character.gridSystem.findPath(character.transform.position, transform.position);
-        if(currentPath != null)
+
+        if (currentPath != null)
+        {
+
+            Color nodeColor = (character.maxRoll >= currentPath.Count ? new Color(1.0f, 1.0f, 1.0f) : new Color(1.0f, 0.0f, 0.0f));
+
             for (int i = currentPath.Count - 1; i > -1; i--)
+            {
                 currentPath[i].GetComponent<MeshRenderer>().enabled = true;
+                currentPath[i].GetComponent<Renderer>().material.color = nodeColor;
+            }
+
+            pathNumber.SetActive(true);
+            pathNumber.transform.position = transform.position + Vector3.up;
+            pathNumber.transform.LookAt(character.transform.position + (Vector3.up * 0.5f));
+
+            pathNumber.GetComponentInChildren<Text>().text = currentPath.Count.ToString();
+
+        }
     }
 
     public override void onLaserDeselected(CharacterScript character)
     {
         if (currentPath != null)
+        {
             for (int i = currentPath.Count - 1; i > -1; i--)
                 currentPath[i].GetComponent<MeshRenderer>().enabled = false;
+            pathNumber.SetActive(false);
+        }
     }
 
     public override void onLaserClick(CharacterScript character, bool clickState)
     {
         //Okay, they clicked this node so let make them start moving to this node!
-        character.MoveTo(transform.position);
+        if(clickState)
+            character.MoveTo(transform.position);
     }
 
 }
