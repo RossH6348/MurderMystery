6418d342280f7f44934f63da53b39f0b4dbc1c60
RossH6348 <75311103+RossH6348@users.noreply.github.com>
2022-06-08 14:08:39 +0100

    Footsteps Test #1

--- Murder Mystery/Assets/Craig/Scripts/Helpers/Helpers.cs
+++ Murder Mystery/Assets/Craig/Scripts/Helpers/Helpers.cs
@@ -3,6 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class GlobalResources
+{
+
+    public static List<AudioClip> footsteps = new List<AudioClip>()
+    {
+        Resources.Load<AudioClip>("Sounds/Footsteps/Footstep1"),
+        Resources.Load<AudioClip>("Sounds/Footsteps/Footstep2"),
+        Resources.Load<AudioClip>("Sounds/Footsteps/Footstep3"),
+        Resources.Load<AudioClip>("Sounds/Footsteps/Footstep4"),
+        Resources.Load<AudioClip>("Sounds/Footsteps/Footstep5")
+    };
+
+}
+
 public class Helpers : MonoBehaviour
 {
     private static Helpers instance;
@@ -29,10 +43,20 @@ public class Helpers : MonoBehaviour
     //This will start a coroutine loop of moving an object through a path.
     public static IEnumerator movePath(List<GameObject> path, Transform transform, float time, Action callback)
     {
+
+        //If we find an audio source attached to VR player or AI player or whatever, we can play some footstep noises.
+        AudioSource source = transform.GetComponent<AudioSource>();
+
         //status = turnStatus.Action; //Set this player to action status.
         while (path.Count > 0)
         {
             GameObject node = path[0];
+
+            if (source != null && GlobalResources.footsteps.Count - 1 > 0)
+            {
+                Debug.Log("PLAY FOOTSTEP!");
+                source.PlayOneShot(GlobalResources.footsteps[UnityEngine.Random.Range(0, GlobalResources.footsteps.Count - 1)]);
+            }
 
             Vector3 currentPos = transform.position;
             float t = 0.0f;
